1481521054a4b88f20c7f78c4c5772d44cec9b25
wajdi <mansourwajdi3@gmial.com>
2023-12-14 17:56:38 +0100

    quiz with timer and warning

--- src/app/quizpage/quiz/quiz.component.ts
+++ src/app/quizpage/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Question } from '../question/question';
 
@@ -22,6 +22,7 @@ export class QuizComponent implements OnInit {
       'Paris'
     ),
   ];
+  remainingTime = 0; // Initial value for the timer
 
   quizForm: FormGroup;
   constructor(private fb: FormBuilder) {
@@ -32,6 +33,26 @@ export class QuizComponent implements OnInit {
     this.questions.forEach((question, index) => {
       this.quizForm.addControl('question' + index, this.fb.control(''));
     });
+    this.startTimer();
+  }
+
+  startTimer() {
+    // Set the initial time (in seconds)
+    this.remainingTime = 60; // in seconds
+    const timerInterval = 1000; // in millisecond
+    // Start the timer
+    const timer = setInterval(() => {
+      this.remainingTime--;
+      if (this.remainingTime <= 0) {
+        clearInterval(timer); // Stop the timer when it reaches 0
+        // You can also add logic to handle the timer reaching 0 here
+      }
+    }, timerInterval);
+  }
+  // HostListener to detect when the user tries to leave the page
+  @HostListener('window:beforeunload', ['$event'])
+  leavingPageAlert($event: any): void {
+      $event.returnValue = 'Are you sure you want to leave? Your progress may be lost.';
   }
 
   onSubmit() {
