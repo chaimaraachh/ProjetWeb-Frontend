8f0e27a6f78ee839283af73463d610903225f002
wajdi <mansourwajdi3@gmial.com>
2023-12-17 10:18:49 +0100

    f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { ScoringdashboardComponent } from './scoringdashboard/scoringdashboard.c
 import { HomedescriptionComponent } from './homedescription/homedescription.component';
 import { RoadmaplistComponent } from './roadmaps/roadmaplist/roadmaplist.component';
 import { QuizComponent } from './quizpage/quiz/quiz.component';
-import { LeavePageGuardService } from './services/leave-page-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -14,7 +13,7 @@ const routes: Routes = [
   { path: 'roadmaps', component: RoadmaplistComponent },
   { path: 'aboutus', component: AboutusComponent },
   { path: 'scoringdashboard', component: ScoringdashboardComponent  },
-  { path: 'quiz', component: QuizComponent  , canDeactivate: [LeavePageGuardService]  },
+  { path: 'quiz', component: QuizComponent  },
 
 ];
 
