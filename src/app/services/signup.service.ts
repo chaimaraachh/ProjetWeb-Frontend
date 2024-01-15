fc2ee66c8a8f6dfbb61782d351e56413c92005e6
wajdi <mansourwajdi3@gmial.com>
2024-01-15 16:33:32 +0100

    signup

---
+++ src/app/services/signup.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { backendApiUrlSignup } from '../config/config';
+import { Observable } from 'rxjs';
+import { NgForm } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor(
+    private http: HttpClient,
+
+  ) { }
+  signup( signUpForm : NgForm):Observable<any>{
+    return this.http.post<any>(backendApiUrlSignup, signUpForm )
+  }
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CountryService } from '../services/country.service';
+import { NgForm } from '@angular/forms';
+import { SignupService } from '../services/signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -14,20 +16,31 @@ export class SignupComponent implements OnInit{
   constructor(
     private router: Router,
     private countryService: CountryService,
+    private signUpService: SignupService
   ) { }
   ngOnInit() {
     this.countryService.getCountries().subscribe(data => {
-      this.countries = data.filter(country => country.name.common !== "Afghanistan");
+
       this.countries = data.sort((a, b) => {
         return a.name.common.localeCompare(b.name.common);
       });
-      console.log(this.countries);
+
     });
   }
   navigateToLogin() {
     this.router.navigate(['/login']);
   }
-  singUp(signUpForm: { value: any; }) {
-    alert(signUpForm.value);
+  singUp(signUpForm:NgForm) {
+    console.log(signUpForm.value);
+    this.signUpService.signup(signUpForm.value).subscribe(
+      (response) => {
+        alert('Vous êtes signé');
+        console.log(response);
+      },
+      (error) => {
+        alert(error.error.message);
+        console.log(error);
+      }
+    )
   }
 }
